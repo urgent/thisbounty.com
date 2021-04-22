66e1549e4c053f0c3b29866ea5692ee50eb73d3e
urgent <urgent.programmer@gmail.com>
2021-04-21 21:04:22 -0400

    #1 -- ChakraProvider, remove styles
    Removed styles/, use Chakra UI theme
    
    Removed elements from main page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import React from "react";
+import { ChakraProvider, Grid } from "@chakra-ui/react";
+import theme from "./theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Grid
+        data-cy="grid"
+        templateColumns="[sidebar] 1fr [content] 3fr [info] 1fr"
+        templateRows={["[nav] 40px [body] auto", "[nav] 100px [body] auto"]}
+        gap={6}
+      >
+        <Component {...pageProps} />
+      </Grid>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
