402018dfc3e0fda8cf2d37f964bcc8fa7070290b
urgent <urgent.programmer@gmail.com>
2021-04-21 21:05:27 -0400

    #1 -- E2E Test grid
    test for visible grid

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,5 +2,6 @@ describe('Homepage', () => {
     it('Homepage exists', () => {
         cy.visit('/')
         cy.get('body').should('be.visible')
+        cy.get('[data-cy=grid]').should('be.visible')
     })
 })
