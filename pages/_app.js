89c95cc41273d18231cccb7267c535bf89e4a8b4
urgent <urgent.programmer@gmail.com>
2021-04-22 11:59:53 -0400

    #1 -- Chakra UI theme move
    Moved from pages to components
    
    next build needs React component as default export

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ChakraProvider, Grid } from "@chakra-ui/react";
-import theme from "./theme";
+import { theme } from "../components/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
