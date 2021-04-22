63b5ab498b974b8cc2e6710d5fb50035c8534c20
urgent <urgent.programmer@gmail.com>
2021-04-22 15:51:39 -0400

    #2 -- Reference
    Remaining code from docs:
    https://next-auth.js.org/getting-started/example

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { ChakraProvider, Grid } from "@chakra-ui/react";
+import { Provider } from "next-auth/client";
 import { theme } from "../components/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Grid
-        data-cy="grid"
-        templateColumns="[sidebar] 1fr [content] 3fr [info] 1fr"
-        templateRows={["[nav] 40px [body] auto", "[nav] 100px [body] auto"]}
-        gap={6}
-      >
-        <Component {...pageProps} />
-      </Grid>
-    </ChakraProvider>
+    <Provider session={pageProps.session}>
+      <ChakraProvider theme={theme}>
+        <Grid
+          data-cy="grid"
+          templateColumns="[sidebar] 1fr [content] 3fr [info] 1fr"
+          templateRows={["[nav] 40px [body] auto", "[nav] 100px [body] auto"]}
+          gap={6}
+        >
+          <Component {...pageProps} />
+        </Grid>
+      </ChakraProvider>
+    </Provider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
 import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function Home() {
+  const [session, loading] = useSession();
+
   return (
     <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <nav>
+        {!session && (
+          <>
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
+          </>
+        )}
+        {session && (
+          <>
+            Signed in as {session.user.email} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        )}
+      </nav>
 
       <main>
         <h1>Welcome!</h1>
