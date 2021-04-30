a7802b6c6b0ce01566da4e26334e50dcdd918234
urgent <urgent.programmer@gmail.com>
2021-04-30 10:38:19 -0400

    #10 -- Test for bounty navigation
    Expect thisbounty to be available for work

--- cypress/integration/bounty.spec.js
+++ cypress/integration/bounty.spec.js
@@ -5,4 +5,10 @@ describe('Bounty', () => {
         cy.get('[data-cy=bounty-work-image]').should('be.visible')
         cy.get('[data-cy=bounty-work-title]').should('be.visible')
     })
+    it('navigates', () => {
+        cy.visit('/bounty/work')
+        cy.get('[data-cy=bounty-thisbounty]').should('be.visible')
+        cy.get('[data-cy=bounty-thisbounty-image]').should('be.visible')
+        cy.get('[data-cy=bounty-thisbounty-title]').should('be.visible')
+    })
 })
