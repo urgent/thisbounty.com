e01dea0f1e34ef8208bc3b6e5c6b0eee23e31088
urgent <urgent.programmer@gmail.com>
2021-05-04 22:39:54 -0400

    #5 -- callback for token, api call in Fork component
    session callback, to return github token to browser, for
    Github API fork. Need to lookup from db, not rely on
    jwt callback per next-auth docs:
    https://next-auth.js.org/configuration/callbacks
    
    Successful API call in Fork component. Needed scope in
    OAuth provider options for repo

--- components/Fork.jsx
+++ components/Fork.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import { Grid, Image, Text, Box } from "@chakra-ui/react";
 import fetch from "isomorphic-fetch";
 
-export async function fork() {
-  const res = await fetch("https://api.github.com/repos/");
+export async function fork(token) {
+  const res = await fetch(
+    "https://api.github.com/repos/urgent/thisbounty.com/forks",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `token ${token}`,
+      },
+    }
+  );
   return res;
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,7 +11,27 @@ const options = {
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      scope: ["repo"],
     }),
   ],
   adapter: Adapters.Prisma.Adapter({ prisma }),
+  callbacks: {
+    /**
+     * @param  {object} session      Session object
+     * @param  {object} token        User object    (if using database sessions)
+     *                               JSON Web Token (if not using database sessions)
+     * @return {object}              Session that will be returned to the client
+     */
+    async session(session, user) {
+      // lookup token from db. Only available in param if using jwt
+      const account = await prisma.account.findMany({
+        where: {
+          userId: user.id,
+        },
+      });
+      // Add property to session, like an access_token from a provider.
+      session.account = { accessToken: account[0].accessToken };
+      return session;
+    },
+  },
 };
